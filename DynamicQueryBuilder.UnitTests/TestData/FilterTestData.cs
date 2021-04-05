4cf825b3350723b7826fcca397b5624456ad502a
Selman Şener <selman.sener@oplog.com.tr>
2021-04-05 11:36:26 +0300

    Change ToLowerInvariant as ToLower

--- DynamicQueryBuilder.UnitTests/TestData/FilterTestData.cs
+++ DynamicQueryBuilder.UnitTests/TestData/FilterTestData.cs
@@ -13,7 +13,7 @@ namespace DynamicQueryBuilder.UnitTests.TestData
                 // Equals
                 // For checking strings
                 yield return new object[] { "(x.Name == \"Test\")", FilterOperation.Equals, true, "Test", "Name" };
-                yield return new object[] { "(x.Name.ToLowerInvariant() == \"test\".ToLowerInvariant())", FilterOperation.Equals, false, "Test", "Name" };
+                yield return new object[] { "(x.Name.ToLower() == \"test\".ToLower())", FilterOperation.Equals, false, "Test", "Name" };
 
                 // For other types
                 yield return new object[] { "(x.InnerMember.Age == 3)", FilterOperation.Equals, true, "3", "InnerMember.Age" };
@@ -21,12 +21,12 @@ namespace DynamicQueryBuilder.UnitTests.TestData
 
                 // Contains
                 yield return new object[] { "x.Name.Contains(\"Test\")", FilterOperation.Contains, true, "Test", "Name" };
-                yield return new object[] { "x.Name.ToLowerInvariant().Contains(\"test\".ToLowerInvariant())", FilterOperation.Contains, false, "Test", "Name" };
+                yield return new object[] { "x.Name.ToLower().Contains(\"test\".ToLower())", FilterOperation.Contains, false, "Test", "Name" };
 
                 // NotEquals
                 // For checking strings
                 yield return new object[] { "(x.Name != \"Test\")", FilterOperation.NotEqual, true, "Test", "Name" };
-                yield return new object[] { "(x.Name.ToLowerInvariant() != \"test\".ToLowerInvariant())", FilterOperation.NotEqual, false, "Test", "Name" };
+                yield return new object[] { "(x.Name.ToLower() != \"test\".ToLower())", FilterOperation.NotEqual, false, "Test", "Name" };
 
                 // For other types
                 yield return new object[] { "(x.InnerMember.Age != 3)", FilterOperation.NotEqual, true, "3", "InnerMember.Age" };
@@ -34,11 +34,11 @@ namespace DynamicQueryBuilder.UnitTests.TestData
 
                 // EndsWith
                 yield return new object[] { "x.Name.EndsWith(\"Test\")", FilterOperation.EndsWith, true, "Test", "Name" };
-                yield return new object[] { "x.Name.ToLowerInvariant().EndsWith(\"test\".ToLowerInvariant())", FilterOperation.EndsWith, false, "Test", "Name" };
+                yield return new object[] { "x.Name.ToLower().EndsWith(\"test\".ToLower())", FilterOperation.EndsWith, false, "Test", "Name" };
 
                 // StartsWith
                 yield return new object[] { "x.Name.StartsWith(\"Test\")", FilterOperation.StartsWith, true, "Test", "Name" };
-                yield return new object[] { "x.Name.ToLowerInvariant().StartsWith(\"test\".ToLowerInvariant())", FilterOperation.StartsWith, false, "Test", "Name" };
+                yield return new object[] { "x.Name.ToLower().StartsWith(\"test\".ToLower())", FilterOperation.StartsWith, false, "Test", "Name" };
 
                 // GreaterThan
                 // For Non-string types
@@ -46,7 +46,7 @@ namespace DynamicQueryBuilder.UnitTests.TestData
                 yield return new object[] { "(x.InnerMember.Age > 3)", FilterOperation.GreaterThan, false, "3", "InnerMember.Age" };
                 //For strings
                 yield return new object[] { "(x.Name.CompareTo(\"TestSix\") > 0)", FilterOperation.GreaterThan, true, "TestSix", "Name" };
-                yield return new object[] { "(x.Name.ToLowerInvariant().CompareTo(\"testsix\".ToLowerInvariant()) > 0)", FilterOperation.GreaterThan, false, "TestSix", "Name" };
+                yield return new object[] { "(x.Name.ToLower().CompareTo(\"testsix\".ToLower()) > 0)", FilterOperation.GreaterThan, false, "TestSix", "Name" };
 
                 // GreaterThanOrEqual
                 // For Non-string types
@@ -54,7 +54,7 @@ namespace DynamicQueryBuilder.UnitTests.TestData
                 yield return new object[] { "(x.InnerMember.Age >= 3)", FilterOperation.GreaterThanOrEqual, false, "3", "InnerMember.Age" };
                 //For strings
                 yield return new object[] { "(x.Name.CompareTo(\"TestSix\") >= 0)", FilterOperation.GreaterThanOrEqual, true, "TestSix", "Name" };
-                yield return new object[] { "(x.Name.ToLowerInvariant().CompareTo(\"testsix\".ToLowerInvariant()) >= 0)", FilterOperation.GreaterThanOrEqual, false, "TestSix", "Name" };
+                yield return new object[] { "(x.Name.ToLower().CompareTo(\"testsix\".ToLower()) >= 0)", FilterOperation.GreaterThanOrEqual, false, "TestSix", "Name" };
 
                 // LessThan
                 // For Non-string types
@@ -62,7 +62,7 @@ namespace DynamicQueryBuilder.UnitTests.TestData
                 yield return new object[] { "(x.InnerMember.Age < 3)", FilterOperation.LessThan, false, "3", "InnerMember.Age" };
                 //For strings
                 yield return new object[] { "(x.Name.CompareTo(\"TestSix\") < 0)", FilterOperation.LessThan, true, "TestSix", "Name" };
-                yield return new object[] { "(x.Name.ToLowerInvariant().CompareTo(\"testsix\".ToLowerInvariant()) < 0)", FilterOperation.LessThan, false, "TestSix", "Name" };
+                yield return new object[] { "(x.Name.ToLower().CompareTo(\"testsix\".ToLower()) < 0)", FilterOperation.LessThan, false, "TestSix", "Name" };
 
                 // LessThanOrEqual
                 // For Non-string types
@@ -70,7 +70,7 @@ namespace DynamicQueryBuilder.UnitTests.TestData
                 yield return new object[] { "(x.InnerMember.Age <= 3)", FilterOperation.LessThanOrEqual, false, "3", "InnerMember.Age" };
                 //For strings
                 yield return new object[] { "(x.Name.CompareTo(\"TestSix\") <= 0)", FilterOperation.LessThanOrEqual, true, "TestSix", "Name" };
-                yield return new object[] { "(x.Name.ToLowerInvariant().CompareTo(\"testsix\".ToLowerInvariant()) <= 0)", FilterOperation.LessThanOrEqual, false, "TestSix", "Name" };
+                yield return new object[] { "(x.Name.ToLower().CompareTo(\"testsix\".ToLower()) <= 0)", FilterOperation.LessThanOrEqual, false, "TestSix", "Name" };
             }
         }
     }
