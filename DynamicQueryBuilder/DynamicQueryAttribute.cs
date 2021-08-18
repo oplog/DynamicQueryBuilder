5b9072aef47df346a5c0f7f2efdba7e33594ae73
Selçuk GÜRAL <selcukgural@gmail.com>
2021-08-18 14:09:14 +0300

    plus character issue fixed (#51)
    Co-authored-by: Selçuk Güral <selcukgural@oplog.com.tr>

--- DynamicQueryBuilder/DynamicQueryAttribute.cs
+++ DynamicQueryBuilder/DynamicQueryAttribute.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Mvc.Filters;
 using System.Collections.Specialized;
 using System.Linq;
 using System.Web;
-
+using DynamicQueryBuilder.Utils;
 using static DynamicQueryBuilder.DynamicQueryBuilderExceptions;
 
 namespace DynamicQueryBuilder
@@ -32,7 +32,6 @@ namespace DynamicQueryBuilder
         /// <param name="maxCountSize">Max data set count for the result set.</param>
         /// <param name="includeDataSetCountToPagination">Includes the total data set count to the options class.</param>
         /// <param name="exceededPaginationCountBehaviour">Behaviour when the requested data set count greater than max count size.</param>
-        /// <param name="resolveFromParameter">Resolves the dynamic query string from the given query parameter value.</param>
         public DynamicQueryAttribute(
             int maxCountSize = 100,
             bool includeDataSetCountToPagination = true,
@@ -56,84 +55,96 @@ namespace DynamicQueryBuilder
                 .GetService(typeof(DynamicQueryBuilderSettings)) as DynamicQueryBuilderSettings
                     ?? new DynamicQueryBuilderSettings();
 
-            if (dynamicQueryParameter != null)
+            if (dynamicQueryParameter == null)
             {
-                string queryValue = context.HttpContext.Request.QueryString.Value;
-                if (dqbSettings.QueryOptionsResolver != null)
-                {
-                    if (dqbSettings.QueryOptionsResolver is QueryStringResolver qsResolver)
-                    {
-                        string[] values;
-                        if (!string.IsNullOrEmpty(qsResolver.ResolveFrom))
-                        {
-                            NameValueCollection resolveParameterValues = HttpUtility.ParseQueryString(context.HttpContext.Request.QueryString.Value);
-                            values = resolveParameterValues.GetValues(qsResolver.ResolveFrom) ?? new string[] { string.Empty };
-                        }
-                        else
-                        {
-                            values = new string[] { queryValue };
-                        }
-
-                        queryValue = HttpUtility.UrlDecode(
-                            qsResolver.DecodeFunction != null
-                            ? qsResolver.DecodeFunction(values[0])
-                            : values[0]);
-                    }
-                    else if (dqbSettings.QueryOptionsResolver is HttpHeaderResolver hhResolver)
-                    {
-                        string rawHeaderValue = context
-                            .HttpContext
-                            .Request
-                            .Headers[hhResolver.HttpHeaderName]
-                            .FirstOrDefault() ?? string.Empty;
-
-                        queryValue = hhResolver.DecodeFunction != null
-                            ? hhResolver.DecodeFunction(rawHeaderValue)
-                            : rawHeaderValue;
-                    }
-                }
-
-                DynamicQueryOptions parsedOptions =
-                    ExpressionBuilder.ParseQueryOptions(
-                        queryValue,
-                        dqbSettings.CustomOpCodes);
-
-                parsedOptions.UsesCaseInsensitiveSource = dqbSettings.UsesCaseInsensitiveSource;
-                parsedOptions.IgnorePredefinedOrders = dqbSettings.IgnorePredefinedOrders;
-                if (parsedOptions.PaginationOption != null)
-                {
-                    parsedOptions.PaginationOption.AssignDataSetCount = _includeDataSetCountToPagination;
-                    if (parsedOptions.PaginationOption.Count > _maxCountSize)
-                    {
-                        if (_exceededPaginationCountBehaviour == PaginationBehaviour.GetMax)
-                        {
-                            parsedOptions.PaginationOption.Count = _maxCountSize;
-                        }
-                        else
-                        {
-                            throw new MaximumResultSetExceededException($"Given count {parsedOptions.PaginationOption.Count} exceeds the maximum amount");
-                        }
-                    }
-                    else if (parsedOptions.PaginationOption.Count <= 0)
-                    {
-                        parsedOptions.PaginationOption.Count = 1;
-                    }
-
-                    if (parsedOptions.PaginationOption.Offset < 0)
-                    {
-                        parsedOptions.PaginationOption.Offset = 0;
-                    }
-                }
-                else if (_includeDataSetCountToPagination && parsedOptions.PaginationOption == null)
-                {
-                    parsedOptions.PaginationOption = new PaginationOption
-                    {
-                        AssignDataSetCount = true
-                    };
-                }
-
-                context.ActionArguments[dynamicQueryParameter.Name] = parsedOptions;
+                base.OnActionExecuting(context);
+                return;
             }
+
+            string queryValue = context.HttpContext.Request.QueryString.Value;
+
+            if (dqbSettings.QueryOptionsResolver != null)
+            {
+                if (dqbSettings.QueryOptionsResolver is QueryStringResolver qsResolver)
+                {
+                    string[] values;
+                    if (!string.IsNullOrEmpty(qsResolver.ResolveFrom))
+                    {
+                        NameValueCollection resolveParameterValues = HttpUtility.ParseQueryString(queryValue);
+                        values = resolveParameterValues.GetValues(qsResolver.ResolveFrom) ?? new string[] {string.Empty};
+                    }
+                    else
+                    {
+                        values = new string[] {queryValue};
+                    }
+
+                    queryValue = values[0];
+
+                    if (qsResolver.DecodeFunction != null)
+                    {
+                        queryValue = qsResolver.DecodeFunction(queryValue);
+                    }
+                    else if (QueryStringParser.IsQueryStringEncoded(queryValue))
+                    {
+                        queryValue = HttpUtility.UrlDecode(queryValue);
+                    }
+
+                }
+                else if (dqbSettings.QueryOptionsResolver is HttpHeaderResolver hhResolver)
+                {
+                    string rawHeaderValue = context
+                        .HttpContext
+                        .Request
+                        .Headers[hhResolver.HttpHeaderName]
+                        .FirstOrDefault() ?? string.Empty;
+
+                    queryValue = hhResolver.DecodeFunction != null
+                        ? hhResolver.DecodeFunction(rawHeaderValue)
+                        : rawHeaderValue;
+                }
+            }
+
+            DynamicQueryOptions parsedOptions =
+                ExpressionBuilder.ParseQueryOptions(
+                    queryValue,
+                    dqbSettings.CustomOpCodes);
+
+            parsedOptions.UsesCaseInsensitiveSource = dqbSettings.UsesCaseInsensitiveSource;
+            parsedOptions.IgnorePredefinedOrders = dqbSettings.IgnorePredefinedOrders;
+            if (parsedOptions.PaginationOption != null)
+            {
+                parsedOptions.PaginationOption.AssignDataSetCount = _includeDataSetCountToPagination;
+                if (parsedOptions.PaginationOption.Count > _maxCountSize)
+                {
+                    if (_exceededPaginationCountBehaviour == PaginationBehaviour.GetMax)
+                    {
+                        parsedOptions.PaginationOption.Count = _maxCountSize;
+                    }
+                    else
+                    {
+                        throw new MaximumResultSetExceededException(
+                            $"Given count {parsedOptions.PaginationOption.Count} exceeds the maximum amount");
+                    }
+                }
+                else if (parsedOptions.PaginationOption.Count <= 0)
+                {
+                    parsedOptions.PaginationOption.Count = 1;
+                }
+
+                if (parsedOptions.PaginationOption.Offset < 0)
+                {
+                    parsedOptions.PaginationOption.Offset = 0;
+                }
+            }
+            else if (_includeDataSetCountToPagination && parsedOptions.PaginationOption == null)
+            {
+                parsedOptions.PaginationOption = new PaginationOption
+                {
+                    AssignDataSetCount = true
+                };
+            }
+
+            context.ActionArguments[dynamicQueryParameter.Name] = parsedOptions;
 
             base.OnActionExecuting(context);
         }
---
+++ DynamicQueryBuilder/Utils/QueryStringParser.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Web;
+
+namespace DynamicQueryBuilder.Utils
+{
+    internal static class QueryStringParser
+    {
+        public static IEnumerable<QueryStringParserResult> GetAllParameterWithValue(string query)
+        {
+            if (string.IsNullOrEmpty(query))
+            {
+                return new QueryStringParserResult[0];
+            }
+
+            var len = query.Length;
+            var parameter = string.Empty;
+
+            var nameValues = new List<QueryStringParserResult>();
+            var order = 0;
+
+            for (var i = 0; i < len; i++)
+            {
+                if (query[i] == 'p' && query[i + 1] == '=')
+                {
+                    var pEndIndex = query.IndexOf("&", i + 2, StringComparison.Ordinal);
+                    parameter = query[(i + 2)..pEndIndex];
+
+                    i = pEndIndex;
+                }
+
+                if (query[i] == 'v' && query[i + 1] == '=')
+                {
+                    var pEndIndex = query.IndexOf("&", i + 2, StringComparison.Ordinal);
+
+                    if (pEndIndex.Equals(-1))
+                    {
+                        nameValues.Add(new QueryStringParserResult(parameter, query[(i + 2)..], order));
+                        break;
+                    }
+
+                    nameValues.Add(new QueryStringParserResult(parameter, query[(i + 2)..pEndIndex], order++));
+
+                    i = pEndIndex;
+                }
+            }
+
+            return nameValues;
+        }
+        internal static bool IsQueryStringEncoded(string query)
+        {
+            var decodedText = HttpUtility.HtmlDecode(query);
+            var encodedText = HttpUtility.HtmlEncode(decodedText);
+
+            return encodedText.Equals(query, StringComparison.OrdinalIgnoreCase);
+        }
+
+        internal static void ReplaceNameValueCollection(IEnumerable<QueryStringParserResult> queryStringParserResults, NameValueCollection queryCollection, string parameterKey)
+        {
+            foreach (var parserResult in queryStringParserResults)
+            {
+                var len = queryCollection.GetValues(parameterKey)?.Length;
+
+                if (!len.HasValue) break;
+
+                var values = queryCollection.GetValues(parameterKey);
+                values?.SetValue(parserResult.Value, parserResult.Order);
+
+                if (values == null)
+                {
+                    continue;
+                }
+
+                queryCollection.Remove(parameterKey);
+
+                foreach (var value in values)
+                {
+                    queryCollection.Add(parameterKey, value);
+                }
+            }
+        }
+    }
+
+    internal class QueryStringParserResult
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public int Order { get; set; }
+
+        public QueryStringParserResult()
+        {
+            
+        }
+        public QueryStringParserResult(string key, string value, int order)
+        {
+            Key = key;
+            Value = value;
+            Order = order;
+        }
+    }
+}
